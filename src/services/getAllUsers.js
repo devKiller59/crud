606fbb496aa926b5984e9538e3f61f9d30a0b74d
devKiller59 <devkiller59@gmail.com>
2026-06-23 04:03:12 -0500

    Update API endpoint for fetching users

--- src/services/getAllUsers.js
+++ src/services/getAllUsers.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getAllUsers = async () =>{
-  const URL = `https://dk59-crud-api.onrender.com/users`;
+  const URL = `https://dk59-crud-api.onrender.com/api/users`;
   const req = await axios.get(URL);
   return req.data
 }
