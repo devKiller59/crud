a3bbce4d31c4d362511022b4d8697722f97c8311
devKiller59 <devkiller59@gmail.com>
2026-06-23 04:03:48 -0500

    Update API endpoint for deleting a user

--- src/services/deleteUser.js
+++ src/services/deleteUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const deleteUser = async(_id) => {
-  const URL = `https://dk59-crud-api.onrender.com/users/${_id}/`;
+  const URL = `https://dk59-crud-api.onrender.com/api/users/${_id}/`;
   const req = await axios.delete(URL);
   return req
 }
