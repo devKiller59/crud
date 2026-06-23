7876d325acf7417ce0fdb93b9e9cfb702d13831f
devKiller59 <devkiller59@gmail.com>
2026-06-23 04:04:04 -0500

    Update API endpoint for user creation

--- src/services/createNewUser.js
+++ src/services/createNewUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const createNewUser = async(data) => {
-  const URL = `https://dk59-crud-api.onrender.com/users`;
+  const URL = `https://dk59-crud-api.onrender.com/api/users`;
   const req = await axios.post(URL, data);
   return req
 }
